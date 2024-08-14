b1d8420075b3ecdd8853c5cfe361a69beae849c1
jyc0011 <jyc0011211@gmail.com>
2024-08-15 00:36:44 +0900

    add : initial setting for react-springboot project

---
+++ src/main/java/com/project/sanhak/SanhakApplication.java
@@ -0,0 +1,13 @@
+package com.project.sanhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanhakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanhakApplication.class, args);
+    }
+
+}
