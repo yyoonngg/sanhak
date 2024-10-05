212d44a6555e19f636a216c30f4d3a7b74f88810
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2024-10-05 16:45:28 +0900

    feat : connect front and backend

---
+++ src/main/java/com/project/sanhak/home/controller/WebConfig.java
@@ -0,0 +1,20 @@
+package com.project.sanhak.home.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .allowedHeaders("Content-Type", "Authorization")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+    }
+}
