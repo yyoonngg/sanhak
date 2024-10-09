1c78ac76a4031544155d7c6b609ad96a97354fa3
권용현 <dydgus9952@naver.com>
2024-10-09 12:52:24 +0900

    feat: 마이페이지 구현
    - 유저 프로필
    - 커스터 로드맵
    - 스킬 상세설명 사이드창

---
+++ src/main/frontend/src/app/mypage/SkillBadge.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type SkillBadgeProps = {
+  skill: UserSkill
+};
+const SkillBadge = ({
+ skill
+}: SkillBadgeProps) => {
+  const image_skill_name = skill.name.toLowerCase().replace(/\s+/g, '').replace(/\./g, ''); // 소문자, 공백제거, "."제거
+  const image_src = `/asset/png/frontend/${image_skill_name}_img.png`;
+  const bg_shadow = 'shadow-[4px_4px_10px_#8247FF,4px_4px_20px_#EF39FF,4px_4px_30px_#FFC839]';
+  
+  return (
+    <div className={`w-24 h-24 flex flex-col items-center justify-between bg-white p-2 m-4 bg-gray-200 rounded-3xl ${bg_shadow}`}>
+      <div className='w-16 h-16'>
+        <img className='w-full h-full object-contain' src={image_src} alt={skill.name}/>
+      </div>
+      <div className='h-4 font-bold text-sm'>{skill.name}</div>
+    </div>
+  )
+};
+
+
+export default SkillBadge;
