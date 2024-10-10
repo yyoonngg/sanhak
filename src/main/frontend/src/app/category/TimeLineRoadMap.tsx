ef28b78bd616e5a53b1341a687d347efd4b9c100
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2024-10-10 19:54:20 +0900

    chore : connect db

--- src/main/frontend/src/app/category/TimeLineRoadMap.tsx
+++ src/main/frontend/src/app/category/TimeLineRoadMap.tsx
@@ -1,7 +1,7 @@
 //컴포넌트1 가로로 정렬된 로드맵 : 들어가야하는 직무별 사진은 href 형태로...?
 //컴포넌트 2 전체 카드 폼 형식 : 직무 카테고리 별 아이콘은 서로 다르게 짤수있도록
 
-//import RoadmapSkill from "@/app/category/Roadmap";
+//import RoadmapSkill from "@/app/categoryDTO/Roadmap";
 
 
 // export enum TimeLineTag{
--- src/main/java/com/project/sanhak/category/controller/categoryController.java
+++ src/main/java/com/project/sanhak/category/controller/categoryController.java
@@ -1,8 +1,25 @@
 package com.project.sanhak.category.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.sanhak.category.service.categoryService;
+import com.project.sanhak.category.dto.categoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/api/v1")
 public class categoryController {
 
+    @Autowired
+    private categoryService categoryService;
+
+    @GetMapping(value = {"/category/{csCate}", "/category"})
+    public List<categoryDTO> getCategories(@PathVariable(required = false) Integer csCate) {
+        if(csCate == null)
+            csCate = 1;
+
+        return categoryService.getSkilNode(csCate);
+    }
 }
