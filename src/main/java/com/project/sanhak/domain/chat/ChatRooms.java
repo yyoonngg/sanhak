57ec622cd11a4a218215d633a8c9ea5d7d36ec3d
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2024-10-19 02:29:33 +0900

    feat : db

---
+++ src/main/java/com/project/sanhak/domain/chat/ChatRooms.java
@@ -0,0 +1,25 @@
+package com.project.sanhak.domain.chat;
+
+import com.project.sanhak.domain.card.ExperienceCard;
+import com.project.sanhak.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class ChatRooms {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int CRId;
+
+    @JoinColumn(referencedColumnName = "ECId")
+    @ManyToOne
+    private ExperienceCard CRecid;
+
+    @JoinColumn(referencedColumnName = "UId")
+    @ManyToOne
+    private User CRuid;
+
+    private String CRRole;
+    private String CRLastmessage;
+}
