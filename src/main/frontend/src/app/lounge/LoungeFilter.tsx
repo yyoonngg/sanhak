844208ccfb770bf53d0b23ce96b1e1d0ecb25f08
권용현 <dydgus9952@naver.com>
2024-11-13 09:16:04 +0900

    feat: career-lounge-page setting

---
+++ src/main/frontend/src/app/lounge/LoungeFilter.tsx
@@ -0,0 +1,52 @@
+'use client';
+import React from 'react';
+
+type LoungeFilterProps = {
+  selectedFilter: string;
+  handleClickFilter: (filter: string) => void
+};
+
+export default function LoungeFilter({
+  selectedFilter,
+  handleClickFilter
+}: LoungeFilterProps) {
+  return (
+    <div className='flex gap-4 mt-2'>
+      <button
+        className={`flex items-center gap-2 px-2 py-1 border rounded-xl hover:bg-gray-d9 ${selectedFilter === "time" ? "bg-primary text-white" : "bg-white"}`}
+        onClick={()=>handleClickFilter('time')}
+      >
+        <img className='w-4' src='asset/png/icon_filter_time.png' alt='최신순' />
+        최신순
+      </button>
+      <button
+        className={`flex items-center gap-2 px-2 py-1 border rounded-xl hover:bg-gray-d9 ${selectedFilter === "click" ? "bg-primary text-white" : "bg-white"}`}
+        onClick={()=>handleClickFilter('click')}
+      >
+        <img className='w-4' src='asset/png/icon_filter_click.png' alt='조회순' />
+        조회순
+      </button>
+      <button
+        className={`flex items-center gap-2 px-2 py-1 border rounded-xl hover:bg-gray-d9 ${selectedFilter === "badge" ? "bg-primary text-white" : "bg-white"}`}
+        onClick={()=>handleClickFilter('badge')}
+      >
+        <img className='w-4' src='asset/png/icon_filter_badge.png' alt='스킬뱃지 많은 순' />
+        스킬뱃지 많은 순
+      </button>
+      <button
+        className={`flex items-center gap-2 px-2 py-1 border rounded-xl hover:bg-gray-d9 ${selectedFilter === "roadmap" ? "bg-primary text-white" : "bg-white"}`}
+        onClick={()=>handleClickFilter('roadmap')}
+      >
+        <img className='w-4' src='asset/png/icon_filter_roadmap.png' alt='커스텀로드맵 많은 순' />
+        커스텀로드맵 많은 순
+      </button>
+      <button
+        className={`flex items-center gap-2 px-2 py-1 border rounded-xl hover:bg-gray-d9 ${selectedFilter === "card" ? "bg-primary text-white" : "bg-white"}`}
+        onClick={()=>handleClickFilter('card')}
+      >
+        <img className='w-4' src='asset/png/icon_filter_card.png' alt='경험카드 많은 순' />
+        경험카드 많은 순
+      </button>
+    </div>
+  );
+}
