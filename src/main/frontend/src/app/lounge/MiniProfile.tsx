c691e39ffa05b0080d2d470f15a201afa54e1d1e
권용현 <dydgus9952@naver.com>
2024-11-24 05:52:51 +0900

    feat: lounge-all api connection

--- src/main/frontend/src/app/lounge/MiniProfile.tsx
+++ src/main/frontend/src/app/lounge/MiniProfile.tsx
@@ -2,11 +2,7 @@
 import React from 'react';
 
 type MiniProfileProps = {
-  name: string;
-  category: string;
-  badge_cnt: number;
-  roadmap_cnt: number;
-  card_cnt: number;
+  profile: MiniProfileInfo
 };
 
 const categoryLabels: Record<string, string> = {
@@ -18,29 +14,25 @@ const categoryLabels: Record<string, string> = {
 };
 
 export default function MiniProfile({
-  name,
-  category,
-  badge_cnt,
-  roadmap_cnt,
-  card_cnt
+  profile
 }: MiniProfileProps) {
   return (
     <div className=' w-64 h-96 transform transition-transform duration-300 hover:scale-105'>
-      <img className='w-64 h-64 object-cover rounded-xl border border-gray-cc' src='asset/png/profile/user_profile_1.png' />
-      <div className='text-xl font-semibold mt-1'>{categoryLabels[category]} 개발자</div>
-      <div className='text-md mt-1'>{name}</div>
+      <img className='w-64 h-64 object-cover rounded-xl border border-gray-cc' src={profile.imageURL} />
+      <div className='text-xl font-semibold mt-1'>{categoryLabels[profile.category]} 개발자</div>
+      <div className='text-md mt-1'>{profile.name}</div>
       <div className='flex mt-1'>
         <div className='flex gap-2 mr-4'>
           <img className='w-6 h-6' src='asset/png/icon_filter_badge.png' />
-          <div>{badge_cnt}</div>
+          <div>{profile.badge_cnt}</div>
         </div>
         <div className='flex gap-2 mr-4'>
           <img className='w-6 h-6' src='asset/png/icon_filter_roadmap.png' />
-          <div>{roadmap_cnt}</div>
+          <div>{profile.roadmap_cnt}</div>
         </div>
         <div className='flex gap-2 mr-4'>
           <img className='w-6 h-6' src='asset/png/icon_filter_card.png' />
-          <div>{card_cnt}</div>
+          <div>{profile.card_cnt}</div>
         </div>
       </div>
     </div>
